841c116fc9d398efef4e65d144d783c48d0d5f59
Grubba27 <70247653+Grubba27@users.noreply.github.com>
2021-10-18 20:00:23 -0300

    added pipes

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,14 +1,48 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class User {
   id: number;
 
+  @IsNotEmpty({
+    message: (validationArguments) => {
+      return returnErrorMessage(validationArguments.targetName);
+    },
+  })
+  @IsString()
   firstName: string;
+
+  @IsNotEmpty({
+    message: (validationArguments) => {
+      return returnErrorMessage(validationArguments.targetName);
+    },
+  })
+  @IsString()
   lastName: string;
+
+  @IsNotEmpty({
+    message: (validationArguments) => {
+      return returnErrorMessage(validationArguments.targetName);
+    },
+  })
+  @IsString()
   fullName: string;
 
+  @IsNotEmpty({
+    message: (validationArguments) => {
+      return returnErrorMessage(validationArguments.targetName);
+    },
+  })
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   password: string;
 
   entryDate: Date;
+}
+
+function returnErrorMessage(field: string) {
+  return `${field} is required`;
 }
 
 export class Users extends Array implements Array<User> {
