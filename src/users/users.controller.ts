9f78ccf100b2437bcfb172160a2803b4076e5f03
Grubba27 <70247653+Grubba27@users.noreply.github.com>
2021-10-18 19:40:00 -0300

    added erorr handleing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import UsersService from './users.service';
 import { User } from './users.entity';
 
 @Controller('users')
 export class UsersController {
-  constructor(private userService: UsersService) {}
+  constructor(private userService: UsersService) {
+  }
 
   @Post()
   public create(@Body() user: User) {
-    const createdUser = this.userService.create(user);
-    return createdUser;
+    return this.userService.create(user);
+  }
+
+  @Get(':userName')
+  public getUserByName(@Param('userName') userName: string) {
+    return this.userService.findUserByName(userName);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { User, Users } from './users.entity';
 
 @Injectable()
@@ -8,5 +8,22 @@ export default class UsersService {
   public create(user: User): User {
     this.users.push(user);
     return user;
+  }
+
+  public findUserByName(userName: string): Users {
+    const foundUser = this.users.filter((user: User) => {
+      return (
+        user.firstName === userName ||
+        user.lastName === userName ||
+        user.fullName === userName
+      );
+    });
+
+    if (foundUser) {
+      return foundUser;
+    } else {
+      throw new HttpException('Not found', HttpStatus.NOT_FOUND);
+    }
+
   }
 }
